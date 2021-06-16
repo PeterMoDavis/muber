76d4e56d7925d304f4a9dcf0cfe0555449f7f730
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-06-15 20:15:32 -0400

    added update route and tested it... added delete route

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -12,4 +12,21 @@ module.exports = {
       .then((driver) => res.send(driver))
       .catch(next);
   },
+
+  edit(req, res, next) {
+    const driverId = req.params.id;
+    const driverProps = req.body;
+
+    Driver.findByIdAndUpdate({ _id: driverId }, driverProps)
+      .then(() => Driver.findById({ _id: driverId }))
+      .then((driver) => res.send(driver))
+      .catch(next);
+  },
+
+  delete(req, res, next) {
+    const driverId = req.params.id;
+    Driver.findByIdAndDelete(driverId)
+      .then((driver) => res.status(204).send(driver))
+      .catch(next);
+  },
 };
